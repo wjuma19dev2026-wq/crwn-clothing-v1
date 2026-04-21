cc92baa7b7f9c081b3ebd3ff4d4268a76dc9d2ce
wjuma19dev2026-wq <wjuma19dev2026@gmail.com>
2026-04-21 17:39:23 -0400

    Agregado los estilos de las etiquetas

--- src/App.js
+++ src/App.js
@@ -7,8 +7,6 @@
  * @property {string} imageUrl
  */
 
-import { Fragment } from "react";
-import CategoryItem from "./components/category-item/category-item.component";
 import Directory from "./components/directory/directory.component";
 
 const App = () => {
@@ -17,27 +15,27 @@ const App = () => {
     {
       id: 1,
       title: "hats",
-      imageUrl: "https://placehold.co/200x200",
+      imageUrl: "https://placehold.co/200x200/67C090/67C090",
     },
     {
       id: 2,
       title: "jackets",
-      imageUrl: "https://placehold.co/200x200",
+      imageUrl: "https://placehold.co/200x200/462C7D/462C7D",
     },
     {
       id: 3,
       title: "sneakers",
-      imageUrl: "https://placehold.co/200x200",
+      imageUrl: "https://placehold.co/200x200/E87F24/E87F24",
     },
     {
       id: 4,
       title: "womes",
-      imageUrl: "https://placehold.co/200x200",
+      imageUrl: "https://placehold.co/400x400/AE2448/AE2448",
     },
     {
-      id: 4,
+      id: 5,
       title: "mens",
-      imageUrl: "https://placehold.co/200x200 ",
+      imageUrl: "https://placehold.co/400x400/FE9EC7/FE9EC7",
     },
   ];
 
--- src/components/category-item/category-item.component.jsx
+++ src/components/category-item/category-item.component.jsx
@@ -1,13 +1,17 @@
+import "./category-item.component.scss";
+
 const CategoryItem = ({ category }) => {
   return (
     <div
       className="category-conainer"
       style={{ background: `url(${category.imageUrl})` }}
     >
-      <h3>{category.title}</h3>
-      <button type="button" className="btn">
-        Shop now
-      </button>
+      <div className="content">
+        <h3>{category.title}</h3>
+        <button type="button" className="btn">
+          Shop now
+        </button>
+      </div>
     </div>
   );
 };
