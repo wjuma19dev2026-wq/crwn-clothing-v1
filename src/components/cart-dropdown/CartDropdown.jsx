f67e74c4493334aeb8acffe1ad29db9dc0c1c29d
wjuma19dev2026-wq <wjuma19dev2026@gmail.com>
2026-04-30 20:44:27 -0400

    Adding clsx plugin

--- src/components/cart-dropdown/CartDropdown.jsx
+++ src/components/cart-dropdown/CartDropdown.jsx
@@ -1,7 +1,14 @@
-import { useContext } from 'react'
-import Button from '../button/Button'
+/** Libraries */
+import clxs from 'clsx'
+
 import styles from './CartDropdown.module.scss'
+
+/** Context */
+import { useContext } from 'react'
 import { CartContext } from '../../context'
+
+/** Components */
+import Button from '../button/Button'
 
 const CartDropdown = () => {
   const onHandlerCheckout = () => {
@@ -12,11 +19,9 @@ const CartDropdown = () => {
 
   return (
     <div
-      className={
-        styles.cardDropdownContainer +
-        ' ' +
-        `${isCartOpen ? styles.open : ''}`
-      }>
+      className={clxs(styles.cardDropdownContainer, {
+        [styles.open]: isCartOpen,
+      })}>
       <div className={styles.btnCheckout}>
         <Button
           fn={onHandlerCheckout}
