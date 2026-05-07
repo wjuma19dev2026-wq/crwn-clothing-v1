daad37955a4bb91b92569e5f8f49df976ee931d3
wjuma19dev2026-wq <wjuma19dev2026@gmail.com>
2026-05-07 11:20:25 -0400

    Creating DirectoryItem and Category Page

---
+++ src/pages/category/Category.jsx
@@ -0,0 +1,35 @@
+import { useContext, useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { ProductCard } from '../../components'
+import { CategoryMapContext } from '../../context'
+import styles from './Category.module.scss'
+
+export const Category = () => {
+  const { category } = useParams()
+  const { categoryMap } = useContext(CategoryMapContext)
+  const [products, setProducts] = useState(
+    categoryMap[category],
+  )
+
+  useEffect(() => {
+    setProducts(categoryMap[category])
+  }, [category, categoryMap])
+
+  return (
+    <div className="container">
+      <div className="row mt-5">
+        <div>
+          <h1 className="text-capitalize">{category}</h1>
+        </div>
+        {products &&
+          products.map(product => (
+            <div
+              className="col-3"
+              key={product.id}>
+              <ProductCard product={product} />
+            </div>
+          ))}
+      </div>
+    </div>
+  )
+}
